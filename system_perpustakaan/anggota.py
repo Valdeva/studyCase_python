6ae138f174bc740756b6c8720da28f4a16927dbf
Tsalatsa <tsalatsadzikri78@gmail.com>
2025-12-12 20:42:01 +0700

    hapus projek karna hilang

--- system_perpustakaan/anggota.py
+++
@@ -1,8 +0,0 @@
-class anggota:
-    def __init__(self, nama, id_anggota, alamat):
-        self.nama = nama
-        self.id_anggota = id_anggota
-        self.alamat = alamat
-
-    def info_anggota(self):
-        return f"Anggota: {self.nama}, ID: {self.id_anggota}, Alamat: {self.alamat}"
--- system_perpustakaan/buku.py
+++
@@ -1,8 +0,0 @@
-class buku:
-    def __init__(self, judul, pengarang, tahun_terbit):
-        self.judul = judul
-        self.pengarang = pengarang
-        self.tahun_terbit = tahun_terbit
-
-    def info_buku(self):
-        return f"{self.judul} oleh {self.pengarang}, diterbitkan pada {self.tahun_terbit}"
